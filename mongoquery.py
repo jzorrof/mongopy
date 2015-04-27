af792128bac2a45ce57ca4abbda63469a90bd6b6
jzorrof@gmail.com <jzorrof@gmail.com>
2015-04-27 15:38:56 +0800

    update insert3

--- mongoquery.py
+++ mongoquery.py
@@ -31,11 +31,18 @@ query by top level field
 query by a field in
 1.an Embedded Document
 2.Aarry
+"""
+# cursor = db.restaurants.find({"address.zipcode":"10075"})
+# for i in cursor:
+#     print i
+# cursor = db.restaurants.find({"grades.grade": "B"})
+# for i in cursor:
+#     print i
+
+"""
+user operator
 { <field1>: { <operator1>: <value1> } }
 """
-cursor = db.restaurants.find({"address.zipcode":"10075"})
-for i in cursor:
-    print i
-cursor = db.restaurants.find({"grades.grade": "B"})
+cursor = db.restaurants.find({"grades.score":{"$lt": 10}})
 for i in cursor:
     print i
