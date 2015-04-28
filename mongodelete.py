151dea54f6ebd2e525e8cf5d9279d1b2a395d2af
jzorrof@gmail.com <jzorrof@gmail.com>
2015-04-28 16:40:51 +0800

    add delete demo

--- mongodelete.py
+++ mongodelete.py
@@ -1,3 +1,30 @@
 __author__ = 'joe_fan'
 # -*- coding: utf-8 -*-
 
+from pymongo import MongoClient
+
+"""
+Create connection
+"""
+client = MongoClient()
+db = client.test
+
+"""
+Remove all documents that match a condition
+"""
+result = db.restaurants.delete_many({"borough": "Manhattan"})
+
+result.deleted_count
+
+"""
+Remove all Documents
+"""
+result = db.restaurants.delete_many({})
+
+result.deleted_count
+
+"""
+Drop a collection
+"""
+db.restaurants.drop()
+
