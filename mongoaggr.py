fa83f6f6f8d8018cb4770fcefbe07adffb479244
jzorrof@gmail.com <jzorrof@gmail.com>
2015-05-06 11:07:40 +0800

    add aggr filter

--- mongoaggr.py
+++ mongoaggr.py
@@ -9,9 +9,26 @@ Aggregate
 client = MongoClient()
 db = client.test
 
+"""
+Group Documents by a Field and Calculate Count
+"""
 cursor = db.restaurants.aggregate(
     [
         {"$group":{"_id":"$borough", "count":{"$sum":1}}}
+    ]
+)
+
+for document in cursor:
+    print(document)
+
+"""
+Filter and Group Documents
+"""
+
+cursor = db.restaurants.aggregate(
+    [
+        {"$match":{"borough":"Queens", "cuisine":"Brazilian"}},
+        {"$group":{"_id":"address.zipcode","count":{"$sum":1}}}
     ]
 )
 
