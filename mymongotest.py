a8952fb6b16b27bdfef319ce2ca093c6af93f465
jzorrof@gmail.com <jzorrof@gmail.com>
2015-05-07 17:22:56 +0800

    fix error

--- mymongotest.py
+++ mymongotest.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 client =MongoClient()
 db = client.chinese
 
-cursor=db.zh.insert({name: "奇虎"})
+cursor=db.zh.insert({"name": "奇虎"})
 
 cursor=db.zh.find()
 for i in cursor:
